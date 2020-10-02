d65be294cf027a2443536f79c8e6bbccdcd1606c
heliospy <helios2844@gmail.com>
2020-10-02 21:16:09 +0530

    added unitest model in funtional_test

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,21 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert "Django" in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+
+    def test_can_start_alist_and_retreive_list(self):
+        self.browser.get('http://localhost:8000/')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finsh the test')
+      
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
