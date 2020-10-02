e95ec767c9e19dfe44e14ee13ba223f85234270f
heliospy <helios2844@gmail.com>
2020-10-02 22:59:35 +0530

    add new test for test and functional test

--- functional_test.py
+++ functional_test.py
@@ -20,19 +20,28 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        testing the input
+      
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
         inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
 
         table = self.browser.find_element_by_id('id_list_tabel')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+        # self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+        #         f'New to-do item did not appera in table. Contensts were:\n{table.text}')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', 
+            [row.text for row in rows])
 
         self.fail('Finish the test!')
       
