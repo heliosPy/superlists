fda0e04c68d338b28f64ef57abeed373fa73c7ac
heliospy <helios2844@gmail.com>
2020-10-02 23:03:54 +0530

    created a util function in functioal test file

--- functional_test.py
+++ functional_test.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_tabel')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
 
 
     def test_can_start_alist_and_retreive_list(self):
@@ -29,19 +34,15 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(2)
 
-        table = self.browser.find_element_by_id('id_list_tabel')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-        #         f'New to-do item did not appera in table. Contensts were:\n{table.text}')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', 
-            [row.text for row in rows])
+        self.check_for_row_in_table('1: Buy peacock feathers')
+        self.check_for_row_in_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
       
