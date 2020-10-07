79fee7ee92af2118d7f5093aa5e1f26acd975a9c
heliospy <helios2844@gmail.com>
2020-10-07 15:04:28 +0530

    functional test for multiusers

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 
-
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -14,9 +14,23 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
     def check_for_row_in_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_tabel')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return 
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
 
 
 
@@ -35,7 +49,7 @@ class NewVisitorTest(LiveServerTestCase):
         )
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
