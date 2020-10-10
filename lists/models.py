349d5987c64931a45f90f9c5bd3b28bbe04c548b
heliospy <helios2844@gmail.com>
2020-10-10 14:33:12 +0530

    added new item model

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
     text = models.CharField(max_length=250)
+    list = models.ForeignKey(List, default=None)
